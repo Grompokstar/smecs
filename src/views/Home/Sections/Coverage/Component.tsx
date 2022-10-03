a9e1495448e43aff2ffd4cb0ce0bac4a1a771605
Roman Chervontsev <roma.chervontsev@gmail.com>
2022-10-04 00:10:32 +0300

    landing (in progress)

--- src/views/Home/Sections/Coverage/Component.tsx
+++ src/views/Home/Sections/Coverage/Component.tsx
@@ -14,6 +14,41 @@ const CoverageSection = (): JSX.Element => {
         <div className={styles.scale_container}>
           <img src="/images/landing/scale.svg" alt="world scale"/>
         </div>
+
+        <h3 className="title-3 mb-30">Publications</h3>
+        <div className={`${styles.subtitle} mb-60`}>
+          SME competitiveness survey results are published as part of the ITC promoting competitiveness series of reports.
+        </div>
+
+        <div className={styles.promo_images_row}>
+          <div className={styles.item}>
+            <img className={styles.image} src='/images/landing/publication_1.png'/>
+            <div className={styles.description}>
+              Promoting SME competitiveness in Africa
+            </div>
+          </div>
+
+          <div className={styles.item}>
+            <img className={styles.image} src='/images/landing/publication_2.png'/>
+            <div className={styles.description}>
+              Promoting SME competitiveness in the Gambia
+            </div>
+          </div>
+
+          <div className={styles.item}>
+            <img className={styles.image} src='/images/landing/publication_3.png'/>
+            <div className={styles.description}>
+              Promoting SME competitiveness in Hungary
+            </div>
+          </div>
+
+          <div className={styles.item}>
+            <img className={styles.image} src='/images/landing/publication_4.png'/>
+            <div className={styles.description}>
+              SME competitiveness in Ghana
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
