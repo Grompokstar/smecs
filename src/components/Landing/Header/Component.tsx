8fb44d39b7ae576e88656de347e0de0fb25a8261
Roman Chervontsev <rc@zen.car>
2022-10-05 09:51:51 +0300

    fix

--- src/components/Landing/Header/Component.tsx
+++ src/components/Landing/Header/Component.tsx
@@ -1,14 +1,18 @@
 import styles                        from './header.module.scss';
+import Link from 'next/link';
 
 const PageHeader = (): JSX.Element => {
   return (
     <div className={styles.root}>
       <div className="page-content">
-        <a href="/">
-          <div className={styles.logo}>
-            <img src="/images/logo.svg" alt="brand logo"/>
-          </div>
-        </a>
+        <Link href="/">
+          <a>
+            <div className={styles.logo}>
+              <img src="/images/logo.svg" alt="brand logo"/>
+            </div>
+          </a>
+        </Link>
+
       </div>
 
     </div>
